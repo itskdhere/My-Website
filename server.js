a28d75ba81900c4b0fa1691c5b413f9ada557b15
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2023-04-12 20:58:23 +0530

    😴

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 3000;
 
 server.disable('x-powered-by');
 server.disable('X-Powered-By');
+server.set('x-powered-by', false);
+server.set('X-Powered-By', false);
 
 server.use("/static", express.static(path.resolve("public", "static")));
 

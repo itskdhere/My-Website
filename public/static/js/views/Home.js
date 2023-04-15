bf43c4a9a6deeb430e8cde0e4616f1e9b7e73bb8
SUBID DAS <itsmesubid@gmail.com>
2023-04-15 15:12:33 +0530

    Minor changes

--- public/static/js/views/Home.js
+++ public/static/js/views/Home.js
@@ -9,29 +9,25 @@ export default class extends AbstractView {
   async getHtml() {
     return `
         <header>
-
         <h1 style="font-family: 'Open Sans'; font-size: 35px" class="logo"><b>itskdhere</b></h1>
-        
         <div class="navbar-below-content">
           <div class="below-box">
             <p class="line-3">
             <b>Connect me on:</b>
             </p>
           </div>
-    
           <div class="links">
-            <a href="https://twitter.com/itskdhere" class="button" style="color:Dodgerblue" title="Twitter"> <i
+            <a href="https://twitter.com/itskdhere" target="_blank" class="button" style="color:Dodgerblue" title="Twitter"> <i
                 class="fa-brands fa-twitter"></i></a>
-            <a href="https://github.com/itskdhere" style="color: black" class="button" title="">
+            <a href="https://github.com/itskdhere" target="_blank" style="color: black" class="button" title="">
               <i class="fa-brands fa-github"></i></a>
-            <a href="https://www.reddit.com/u/itskdhere" style="color: red" class="button" title="Github"> <i
+            <a href="https://www.reddit.com/u/itskdhere" target="_blank" style="color: red" class="button" title="Github"> <i
                 class="fa-brands fa-reddit"></i></a>
-            <a href="https://discord.gg/9CDh4Qh6vz" style="color: blue" class="button" title="Discord">
+            <a href="https://discord.gg/9CDh4Qh6vz" target="_blank" style="color: blue" class="button" title="Discord">
               <i class="fa-brands fa-discord"></i></a>
           </div>
         </div>
       </header>
-    
       <main>
         <h3>Hi, I'm <b>KD</b></h3>
         <p>
